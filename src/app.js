14f903f3b2b5bd2eeaaf3f17feff3b0f5d16dbb3
Chrissy-coder <christine.soropa@gmail.com>
2023-06-30 10:20:46 +1200

    Worked on J.S for search engine to work.

--- src/app.js
+++ src/app.js
@@ -7,9 +7,19 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.condition.description;
 }
 
-let apiKey = "8f63022fa4b60b22c3t7fe8f63b45ob9";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=New-York&key=8f63022fa4b60b22c3t7fe8f63b45ob9&units=metric";
+function search(city) {
+  let apiKey = "8f63022fa4b60b22c3t7fe8f63b45ob9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=8f63022fa4b60b22c3t7fe8f63b45ob9&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
